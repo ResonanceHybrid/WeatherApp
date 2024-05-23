84e9c8eaed409b26a62dd676e2156c5b71dcb944
Sohan <sosohan404@gmail.com>
2024-05-23 18:10:18 +0545

    UI Improved

--- lib/weatherpage.dart
+++ lib/weatherpage.dart
@@ -3,41 +3,44 @@ import 'package:lottie/lottie.dart';
 import 'package:weatherapp/weathermodel.dart';
 import 'package:weatherapp/weatherservice.dart';
 
-class Weatherpage extends StatefulWidget {
-  const Weatherpage({super.key});
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
 
   @override
-  State<Weatherpage> createState() => _WeatherpageState();
+  State<WeatherPage> createState() => _WeatherPageState();
 }
 
-class _WeatherpageState extends State<Weatherpage> {
-  final _weatherservie = Weatherservice('37a2aecbfa9212f6867052dbce6be326');
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = Weatherservice('37a2aecbfa9212f6867052dbce6be326');
   Weather? _weather;
-  bool _loading = true;
+  bool _isLoading = false;
 
-  Future<void> _fetchWeather() async {
+  _fetchWeather() async {
     setState(() {
-      _loading = true;
+      _isLoading = true;
     });
-    await Future.delayed(
-        Duration(milliseconds: 500)); // Adding delay for loading effect
-    try {
-      String cityName = await _weatherservie.getCurrentCity();
-      print('Fetched City Name: $cityName');
-      if (cityName.isNotEmpty && cityName != 'Unknown') {
-        final weather = await _weatherservie.getWeather(cityName);
+    String cityName = await _weatherService.getCurrentCity();
+    print('Fetched City Name: $cityName');
+
+    if (cityName.isNotEmpty && cityName != 'Unknown') {
+      try {
+        final weather = await _weatherService.getWeather(cityName);
         setState(() {
           _weather = weather;
+          _isLoading = false;
         });
-      } else {
-        print('Failed to get city name');
+      } catch (e) {
+        print(e);
+        setState(() {
+          _isLoading = false;
+        });
       }
-    } catch (e) {
-      print(e);
+    } else {
+      print('Failed to get city name');
+      setState(() {
+        _isLoading = false;
+      });
     }
-    setState(() {
-      _loading = false;
-    });
   }
 
   String getWeatherAnimation(String? mainCondition) {
@@ -70,69 +73,133 @@ class _WeatherpageState extends State<Weatherpage> {
     _fetchWeather();
   }
 
+  Future<void> _refreshWeather() async {
+    await _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather App'),
-        centerTitle: true,
-      ),
-      body: RefreshIndicator(
-        onRefresh: _fetchWeather,
-        child: _loading
-            ? Center(child: CircularProgressIndicator())
+      backgroundColor: Colors.grey[800], // Background color set to dark gray
+      body: Center(
+        child: _isLoading
+            ? Lottie.asset(
+                'assets/loading2.json',
+                height: 300,
+                width: 300,
+                repeat: true,
+              ) // Custom loading animation
             : SingleChildScrollView(
-                physics: AlwaysScrollableScrollPhysics(),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 40.0),
-                    child: Column(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 50),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        Icon(Icons.location_on,
+                            color: Colors.white), // Location icon
                         Text(
-                          _weather?.cityName ?? 'Fetching city...',
+                          _weather?.cityName ?? 'City...',
                           style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 20),
-                        Lottie.asset(
-                          getWeatherAnimation(_weather?.Condition),
-                          height: 200,
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          _weather != null
-                              ? '${_weather!.temperature.round()}°C'
-                              : 'Fetching temperature...',
-                          style: TextStyle(fontSize: 32),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          _weather != null
-                              ? 'Pressure: ${_weather!.pressure.round()} hPa'
-                              : 'Fetching pressure...',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          _weather != null
-                              ? 'Humidity: ${_weather!.humidity.round()}%'
-                              : 'Fetching humidity...',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          _weather?.Condition ?? "Fetching condition...",
-                          style: TextStyle(
-                              fontSize: 18, fontStyle: FontStyle.italic),
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ],
                     ),
-                  ),
+                    SizedBox(height: 20),
+                    Lottie.asset(
+                      getWeatherAnimation(_weather?.Condition),
+                      height: 200,
+                      width: 200,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.thermostat_outlined,
+                            color: Colors.white), // Temperature icon
+                        SizedBox(width: 5),
+                        Text(
+                          '${_weather?.temperature.round()}°C',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildInfoBox(
+                          icon: Icons.opacity_outlined,
+                          label: 'Humidity',
+                          value: '${_weather?.humidity.round()}%',
+                        ),
+                        _buildInfoBox(
+                          icon: Icons.cloud_outlined,
+                          label: 'Pressure',
+                          value: '${_weather?.pressure.round()}hPa',
+                        ),
+                        _buildInfoBox(
+                          icon: Icons.waves_outlined,
+                          label: 'Condition',
+                          value: _weather?.Condition ?? "",
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 50),
+                    ElevatedButton.icon(
+                      onPressed: _fetchWeather,
+                      icon: Icon(Icons.refresh,
+                          color: Colors.white), // Icon color set to white
+                      label: Text(
+                        'Refresh Weather',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal:
+                                20), // Padding around the button content
+                        backgroundColor:
+                            Colors.grey[700], // Background color of the button
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10), // Rounded corners for the button
+                        ),
+                      ),
+                    ),
+
+                    // SizedBox(height: 50),
+                  ],
                 ),
               ),
       ),
+    );
+  }
+
+  Widget _buildInfoBox(
+      {required IconData icon, required String label, required String value}) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.white),
+        SizedBox(height: 5),
+        Text(
+          label,
+          style: TextStyle(color: Colors.white),
+        ),
+        Text(
+          value,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
     );
   }
 }
